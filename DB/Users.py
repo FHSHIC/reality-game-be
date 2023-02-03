9ec617991b230be424a0455ce47d4d6789f6f701
cxphoenix <jate9229@gmail.com>
2023-02-03 14:24:41 +0800

    feat: 新增產生  AccessToken 方法

--- DB/Users.py
+++ DB/Users.py
@@ -1,7 +1,14 @@
 from typing import List, Union
 from pydantic import BaseModel
+import hashlib
 from __DetaDB import DataBase
 
 class UserDB:
     def __init__(self):
         return DataBase().Base("users")
+    
+    @staticmethod
+    def generateAccessToken(email: str, expiredTime: str):
+        access = hashlib.sha256()
+        access.update(f"{email}-{expiredTime}")
+        return access.hexdigest()
