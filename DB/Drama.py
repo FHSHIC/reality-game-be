d04d4f27e1b89acfe3e62a923c6c7f3a658ec1f3
CXPhoenix <jate9229@gmail.com>
2023-02-14 17:58:52 +0800

    fix: 🐛 刪除無用空格

--- DB/Drama.py
+++ DB/Drama.py
@@ -2,10 +2,6 @@ from typing import List, Union
 from pydantic import BaseModel
 from DetaDB import DetaBase
 
-
-
-
-
 class DramaDB:
     def __init__(self):
         return DetaBase().Base("dramas")
