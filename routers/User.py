59c1f0aa0fe33e9371760af0ce3b8a98f191e7e3
CXPhoenix <jate9229@gmail.com>
2023-02-28 20:38:47 +0800

    feat(routers/User.py): 新增了 login

--- routers/User.py
+++ routers/User.py
@@ -1,7 +1,16 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter(prefix="/user")
+
+class UserLogin(BaseModel):
+    account: str
+    password: str
 
 @router.get("/me")
 async def getCurrentUser():
     return {"user": "me"}
+
+@router.post("/login")
+async def login(user: UserLogin):
+    return user.dict()
