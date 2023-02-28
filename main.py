a620ae2bf84ce7a4fd27dfb6c42a8a73671e7f19
CXPhoenix <jate9229@gmail.com>
2023-03-01 01:04:52 +0800

    feat: 🎸 router 加入 app 中

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # -*- coding: UTF-8 -*-
 from fastapi import FastAPI
-from routers import user
+from routers import user, team
 
 app = FastAPI(root_path="/api")
 
 app.include_router(user.router)
+app.include_router(team.router)
 
 
 @app.get("/")
