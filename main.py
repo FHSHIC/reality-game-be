3243d787fd6b878281acba047a9aa88016c0f00a
CXPhoenix <jate9229@gmail.com>
2023-05-16 05:29:20 +0800

    refactor: 💡 重構 team

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import user, team, drama, level, hint, upload
+from routers import team, user, drama, level, hint, upload
 
 app = FastAPI(root_path="/api")
 
